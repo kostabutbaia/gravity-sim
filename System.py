fix of gravity formula and added energy in System

System.py
@@ -5,7 +5,7 @@ from functools import reduce
 
 G = 1
 def get_acceleration(ob1: Object, ob2: Object) -> list[float]:
-    return -G * ob2.m/np.linalg.norm(ob1.r - ob2.r) * (ob1.r - ob2.r)
+    return -G * ob2.m/np.linalg.norm(ob1.r - ob2.r)**3 * (ob1.r - ob2.r)
 
 class System:
     def __init__(self, objects: list[Object], t_final: float, t_step: float, follow_center: bool):
@@ -29,6 +29,15 @@ class System:
             x_cords.append(o.r[0])
             y_cords.append(o.r[1])
         return [x_cords, y_cords]
+    
+    def get_energy(self):
+        E_k = sum([o.m*np.linalg.norm(o.v)**2/2 for o in self.objects])
+        E_p = 0
+        for o1 in self.objects:
+            for o2 in self.objects:
+                if o1 is not o2:
+                    E_p += G*o1.m*o2.m/np.linalg.norm(o1.r-o2.r)
+        return E_k + 1/2*E_p
 
     def _update_object(self, ob: Object):
         for o in self.objects:
@@ -45,10 +54,12 @@ class System:
         if self.follow_center: self._follow_mass_center()
         t_range = np.arange(0, self.t_final, self.t_step)
         frames = []
+        energies=[]
         for t in t_range:
             frames.append(self.get_system_pos_state())
             self._update_system()
-        return t_range, frames
+            energies.append(self.get_energy())
+        return energies, t_range, frames
 
 
 def main():

anim.py
@@ -6,17 +6,17 @@ from System import *
 from Object import *
 from Trail import *
 
-NAME = 'not_follow_2'
-TRAIL_LENGTH = 10
+NAME = 'test3'
+TRAIL_LENGTH = 30
 
 def create_anim_gif(name: str, system: System) -> None:
     fig = plt.figure()
-    plt.xlim(-5, 5)
-    plt.ylim(-5, 5)
+    plt.xlim(-2, 2)
+    plt.ylim(-1, 3)
     plt.grid()
     plt.gca().set_aspect('equal')
 
-    _, frames = system.get_frames()
+    energies, _, frames = system.get_frames()
     l, = plt.plot([], [], 'o')
 
     trail_plots = []
@@ -27,11 +27,11 @@ def create_anim_gif(name: str, system: System) -> None:
         trail_plots.append(p)
     
     writer = PillowWriter(fps=15)
-
     with writer.saving(fig, f'solutions/{name}.gif', 100):
-        for frame in frames:
+        for k, frame in enumerate(frames):
             x_points, y_points = frame
             l.set_data(x_points, y_points)
+            plt.title(f"energy: {round(energies[k], 5)}", loc = 'left')
             for i in range(len(frame)):
                 x_trail, y_trail = trails[i].update_trail(x_points[i], y_points[i])
                 trail_plots[i].set_data(x_trail, y_trail)
@@ -41,8 +41,8 @@ def create_anim_gif(name: str, system: System) -> None:
 
 if __name__ == '__main__':
     objects = [
-        Object(100, np.array([0, 0], dtype='float64'), np.array([0, 0], dtype='float64')),
-        Object(1, np.array([1, 0], dtype='float64'), np.array([0, np.sqrt(100)], dtype='float64'))
+        Object(10, np.array([0, 0], dtype='float64'), np.array([0, -0.2], dtype='float64')),
+        Object(1, np.array([1, 0], dtype='float64'), np.array([0, np.sqrt(10)], dtype='float64'))
     ]
     sys = System(objects, 10, 0.02, False)
     create_anim_gif(NAME, sys)